c4b0178008e948ee7dc792472777146c67e2d281
xbella01 <62183961+xbella01@users.noreply.github.com>
2022-04-24 12:21:56 +0200

    Added comments
    Added comments for program documentation in Doxygen

--- src/math_lib.py
+++ src/math_lib.py
@@ -5,29 +5,58 @@
 # @date April 2022
 ##
 
+# Imports
 import math
 
+# Global Constants
+## value of Pi
 pi = math.pi
 
 
+# Functions
+##
+# @brief Function for addition of 2 numbers
+# @param x augend
+# @param y addend
+# @return sum
 def add(x, y):
     return round(x + y, 5)
 
 
+##
+# @brief Function for subtraction of 2 numbers
+# @param x minuend
+# @param y subtrahend
+# @return difference
 def sub(x, y):
     return round(x - y, 5)
 
 
+##
+# @brief Function for multiplication of 2 numbers
+# @param x multiplier
+# @param y multiplicand
+# @return product
 def mul(x, y):
     return round(x * y, 5)
 
 
+##
+# @brief Function for division of 2 numbers
+# @param x dividend
+# @param y divisor
+# @return ratio
 def div(x, y):
     if y == 0:
         raise ZeroDivisionError
     return round(x / y, 5)
 
 
+##
+# @brief Function for modulo
+# @param x dividend
+# @param y divisor
+# @return remainder
 def mod(x, y):
     if y == 0:
         raise ZeroDivisionError
@@ -45,6 +74,11 @@ def mod(x, y):
         return round(x % (-y), 5)
 
 
+##
+# @brief Function for exponentiation of 2 numbers
+# @param x base
+# @param y exponent
+# @return power
 def pow(x, y):
     if y < 0:
         raise ValueError
@@ -53,6 +87,11 @@ def pow(x, y):
     return round(x**y, 5)
 
 
+##
+# @brief Function for root of 2 numbers
+# @param x radicand
+# @param y degree
+# @return root
 def root(x, y):
     if x < 0 and y % 2 == 0:
         raise ValueError
@@ -65,27 +104,47 @@ def root(x, y):
     return round(x**(1/y), 5)
 
 
+##
+# @brief Function for goniometric function sinus
+# @param x angle in radians
+# @return sinus
 def sin(x):
     return round(math.sin(x), 5)
 
 
+##
+# @brief Function for goniometric function cosinus
+# @param x angle in radians
+# @return cosinus
 def cos(x):
     return round(math.cos(x), 5)
 
 
+##
+# @brief Function for goniometric function tangens
+# @param x angle in radians
+# @return tangens
 def tan(x):
     not_defined = 2*x/math.pi
     if not_defined.is_integer() and not_defined % 2 != 0:
         raise ValueError
     return round(math.tan(x),5)
-    
 
+
+##
+# @brief Function for logarithm with base 10
+# @param x antilogarithm
+# @return logarithm
 def log(x):
     if x <= 0:
         raise ValueError
     return round(math.log10(x), 5)
 
 
+##
+# @brief Function for factorial
+# @param x input of factorial
+# @return factorial
 def factorial(x):
     if x < 0:
         raise ValueError
