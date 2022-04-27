8e99144f299d97cdebe26a1426a5f3c7ec72fb58
mathew35 <matus549@gmail.com>
2022-04-27 12:50:40 +0200

    profiling.py - changed function of profiling to calculate deviation

--- src/profiling.py
+++ src/profiling.py
@@ -14,43 +14,41 @@ import cProfile
 ##
 # @brief function using all functions from math_lib
 # @param FILE file filled with values for math_lib functions
-def func(FILE):
+def deviation(FILE):
     sum = 0
-    subtr = 0
-    square = 0
-    root = 0
+    squaresum = 0
+    N = 0
     for line in FILE:
-        sum = ml.add(float(line),ml.mul(sum,2))
-        subtr = ml.sub(ml.mul(subtr,2),float(line))
-        square = ml.add(ml.pow(float(line),2),ml.div(square,2))
-        root = ml.sub(ml.root(float(line),2),ml.div(root,2))
-        modulo = ml.mod(round(square),9)
-        trigoniometry = ml.cos(ml.tan(ml.sin(float(line))))
-        factor = ml.factorial(round(float(line)))
-        logarithm = ml.log(float(line))
-    return sum
+        sum = ml.add(float(line),sum)
+        squaresum = ml.add(ml.pow(float(line),2),squaresum)
+        N += 1
+    mean = ml.div(sum,N)
+    meanSquared = ml.pow(mean,2)
+    NmeanSquared = ml.mul(N,meanSquared)
+    dev = ml.root(ml.mul(ml.div(1,ml.sub(N,1)),NmeanSquared),2)
+    return dev
 
 if __name__ == '__main__' :
     print("#Test 1 - test 10 values",end='\n\n')
     FILE = open('../profiling/10.txt')
-    cProfile.run('func(FILE)')
+    cProfile.run('deviation(FILE)')
     print("########################################################################\n")
     print("#Test 2 - test 100 values",end='\n\n')
     FILE = open('../profiling/100.txt')
-    cProfile.run('func(FILE)')
+    cProfile.run('deviation(FILE)')
     print("########################################################################\n")
     print("#Test 3 - test 1 000 values",end='\n\n')
     FILE = open('../profiling/1_000.txt')
-    cProfile.run('func(FILE)')
+    cProfile.run('deviation(FILE)')
     print("########################################################################\n")
     print("#Test 4 - test 10 000 values",end='\n\n')
     FILE = open('../profiling/10_000.txt')
-    cProfile.run('func(FILE)')
+    cProfile.run('deviation(FILE)')
     print("########################################################################\n")
     print("#Test 5 - test 100 000 values",end='\n\n')
     FILE = open('../profiling/100_000.txt')
-    cProfile.run('func(FILE)')
+    cProfile.run('deviation(FILE)')
     print("########################################################################\n")
     print("#Test 5 - test 1 000 000 values",end='\n\n')
     FILE = open('../profiling/1_000_000.txt')
-    cProfile.run('func(FILE)')
+    cProfile.run('deviation(FILE)')
